f6faba34c7ac64964d9089d0438fe813b64760d9
azukauteh <aazure263@gmail.com>
2023-02-17 15:32:54 -0800

    program that prints all possible different combinations of three digits.

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,43 +1,54 @@
-/*
- * File: 101-print_comb4.c
- * Auth: azuka uteh
- */
-
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
- * main - Prints all possible combinations of three different digits,
- *        in ascending order, separated by a comma followed by a space.
- *
- * Return: Always 0.
+ * main - main block
+ * Description: Write a program that prints all possible
+ * different combinations of two digits
+ * Numbers must be separated by ,, followed by a space
+ * The two digits must be different
+ * 01 and 10 are considered the same combination of the two digits 0 and 1
+ * Print only the smallest combination of two digits
+ * Numbers should be printed in ascending order, with two digits
+ * You can only use the putchar function
+ * (every other function (printf, puts, etc…) is forbidden)
+ * You can only use putchar five times maximum in your code
+ * You are not allowed to use any variable of type char
+ * All your code should be in the main function
+ * Return: 0
  */
 int main(void)
 {
 	int c;
-	int d = 0;
+	int d;
+	int e = 0;
 
-	while (d < 10)
+	while (e < 10)
 	{
-		c = 0;
-		while (c < 10)
+		d = 0;
+		while (d < 10)
 		{
-			if (d != c && d < c)
+			c = 0;
+			while (c < 10)
 			{
-				putchar('0' + d);
-				putchar('0' + c);
-
-				if (c + d != 17)
+				if (c != d && d != e && e < d && d < c)
 				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
+					putchar('0' + e);
+					putchar('0' + d);
+					putchar('0' + c);
 
-			c++;
+					if (c + d + e != 9 + 8 + 7)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+
+				c++;
+			}
+			d++;
 		}
-		d++;
+		e++;
 	}
 	putchar('\n');
 	return (0);
 }
-
