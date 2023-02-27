635e92debcb2f9b51416954862365f99733e7b14
azukauteh <aazure263@gamil.com>
2023-02-27 11:15:20 -0800

    a function that swaps the values of two integers.

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,16 +1,24 @@
-/*
- * File: 1-swap.c
- * Auth: azuka uteh
- * swap_int - Swaps the values of two integers.
- * @a: The first integer to be swapped.
- * @b: The second integer to be swapped.
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
-
-#include "Main.h"
-
-void swap_int(int *a, int *b)
+int main(void)
 {
-	int tmp = *a;
-	*a = *b;
-	*b = tmp;
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if ((n % 10) == 0)
+		printf("Last digit of %d is %d and is 0\n", n, n % 10);
+	else if ((n % 10) > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+	else
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+
+	return (0);
 }
