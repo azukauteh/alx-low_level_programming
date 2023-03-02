ecffe2a7710e8823bfa299830ef4961a1371b92d
azukauteh <aazure263@gamil.com>
2023-03-02 01:14:04 -0800

    a function that prints an integer

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,6 +10,7 @@
 
 #include <stddef.h>
 
+int _putchar(char c);
 char *_strcat(char *idest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
