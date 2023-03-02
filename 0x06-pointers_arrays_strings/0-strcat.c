d1d854c52c2c17a799f4d2db1dc1dd8992e00747
azukauteh <aazure263@gamil.com>
2023-03-02 02:25:01 -0800

    a function that concatenates two strings.

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,26 +1,30 @@
-/*
- * File: 0-memset.c
- * Auth: azuka uteh
- */
-
 #include "main.h"
 
 /**
- * memset - Fills the first n bytes of the memory area
- *          pointed to by @s with the constant byte @c.
- * @s: A pointer to the memory area to be filled.
- * @c: The character to fill the memory area with.
- * @n: The number of bytes to be filled.
- *
- * Return: A pointer to the filled memory area @s.
+ * _strcat - functions that concatenates two strings.
+ * @src: string to copy
+ * @dest: destination of string.
+ * Return: dest.
  */
-void *_memset(void *s, int c, size_t n)
+
+char *_strcat(char *dest, char *src)
 {
-	unsigned int index;
-	unsigned char *memory = s, value = c;
+	int i;
+	int j;
 
-	for (index = 0; index < n; index++)
-		memory[index] = value;
-
-	return (memory);
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
+
