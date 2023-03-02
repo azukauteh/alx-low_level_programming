ef8c908287fe04849db930c0d61f0b19b39b801b
azukauteh <aazure263@gamil.com>
2023-03-02 01:10:48 -0800

    a function that prints an integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 /**
- * file: 101-print_number.c
  * base10 - power in 10 base
  * @n: an exponent
  * Return: returns 10 to power exponent
@@ -16,38 +15,4 @@ int base10(int n)
 		n--;
 	}
 	return (base);
-}
-
-/**
- * print_number - prints integers enters as parameters using putchar
- * @n: integer to print
- * Return: void
- */
-void print_number(int n)
-{
-	int power;
-
-	power = base10(8);
-
-	if (n < 0)
-	{
-		_putchar('-');
-		n *= -1;
-	}
-
-	if (n == 0)
-		_putchar('0');
-
-	else
-	{
-		while (n / power == 0)
-			power /= 10;
-
-		while (power >= 1)
-		{
-			_putchar((n / power) + '0');
-			n %= power;
-			power /= 10;
-		}
-	}
 }
