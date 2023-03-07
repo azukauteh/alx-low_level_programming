ec993d282bfcb899fbc47d432a1e592c2c5dd82f
azukauteh <aazure263@gamil.com>
2023-03-07 01:06:03 -0800

    a function that gets the length of a prefix substring.

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,12 +1,10 @@
 #include "main.h"
 
 /**
- * main - check the code
- * @s: pointer to char
- * @c: char params to found
- * Return: 0
+ * _strchr - check the code
+ * 
+ * Return: Always 0.
 */
-
 char *_strchr(char *s, char c)
 {
 	while (*s != '\0')
