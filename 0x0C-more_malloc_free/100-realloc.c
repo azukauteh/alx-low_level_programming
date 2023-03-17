4e5c9df411e27008ecf866979ab0353e6f5a1064
azukauteh <aazure263@gamil.com>
2023-03-17 00:10:25 -0700

    a program that multiplies two positive numbers.

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,56 +1,48 @@
-
-#include "main.h"
 #include <stdlib.h>
-
+#include <stdio.h>
 /**
- * _realloc - Reallocates a memory block using malloc and free.
- * @ptr: A pointer to the memory previously allocated.
- * @old_size: The size in bytes of the allocated space for ptr.
- * @new_size: The size in bytes for the new memory block.
+ * _realloc - reallocates memory using malloc and free
+ * @ptr: pointer to the memory previously allocated with a
+ * call to ``` malloc : malloc(old_size) ```.
+ * @old_size: size in bytes allocated for ptr.
+ * @new_size: size in bytes of new memory block.
  *
- * Return: If new_size == old_size - ptr.
- *         If new_size == 0 and ptr is not NULL - NULL.
- *         Otherwise - a pointer to the reallocated memory block.
+ * Return: pointer to new mem block, NULL or ptr.
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *mem;
-	char *ptr_copy, *filler;
-	unsigned int index;
+	unsigned int i;
+	char *p, *arr_old;
 
-	if (new_size == old_size)
-		return (ptr);
-
-	if (ptr == NULL)
+	if (ptr != NULL)
 	{
-		mem = malloc(new_size);
-
-		if (mem == NULL)
-			return (NULL);
-
-		return (mem);
+		arr_old = ptr;
 	}
-
+	else
+	{
+		return ((malloc(new_size)));
+	}
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
-
-	ptr_copy = ptr;
-	mem = malloc(sizeof(*ptr_copy) * new_size);
-
-	if (mem == NULL)
+	if (new_size == old_size)
 	{
-		free(ptr);
-		return (NULL);
+		return (ptr);
 	}
-
-	filler = mem;
-
-	for (index = 0; index < old_size && index < new_size; index++)
-		filler[index] = *ptr_copy++;
-
-	free(ptr);
-	return (mem);
+	if (new_size > old_size)
+	{
+		p = malloc(new_size);
+		if (p == NULL)
+		{
+			return (p);
+		}
+		for (i = 0; i < old_size; i++)
+		{
+			p[i] = arr_old[i];
+		}
+		free(arr_old);
+	}
+	return (p);
 }
