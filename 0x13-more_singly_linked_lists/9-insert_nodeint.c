bb1be2eed509cefe95a3b7d39aaf34b169bca75f
azukauteh <aazure263@gmail.com>
2023-04-04 07:40:56 -0700

    a function that inserts a new node at a given position.

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -8,7 +8,6 @@
  * @n: Data of the new node.
  * Return: Address of the new node.
  **/
-
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 listint_t *new_node, *temp;
