afec737299bc3132a141f9565adbba637e84cc48
azukauteh <aazure263@gmail.com>
2023-04-04 03:11:26 -0700

    lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,11 +1,8 @@
-#ifndef LIST_H
-#define LIST_H
+#ifndef LISTS_H
+#define LISTS_H
 
-/*
- * @list.h file contains all prototypes.
-*/
-
-#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 /**
  * struct listint_s - singly linked list
@@ -13,15 +10,14 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
  */
-
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
-} listint_t;
 
+int n;
+struct listint_s *next;
+
+} listint_t;
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
@@ -35,7 +31,5 @@ int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 
-#endif
-
-
+#endif /* LISTS_H */
 
