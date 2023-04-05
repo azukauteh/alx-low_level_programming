1ca30eba614852df6c05f5c96aa76c832084685d
azukauteh <aazure263@gmail.com>
2023-04-05 08:49:45 -0700

    a function that inserts a new node at a given position.

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 listint_t *new, *copy = *head;
 unsigned int node;
-
 new = malloc(sizeof(listint_t));
 if (new == NULL)
 return (NULL);
@@ -23,7 +22,7 @@ if (idx == 0)
 {
 new->next = copy;
 *head = new;
-return (new);	
+return (new);
 }
 
 for (node = 0; node < (idx - 1); node++)
