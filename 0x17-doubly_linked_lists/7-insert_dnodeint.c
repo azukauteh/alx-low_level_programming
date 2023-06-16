1cb291faefcba57f3e101d01091e34f85b997048
azukauteh <aazure263@gmail.com>
2023-06-16 14:18:15 +0000

    a function that inserts a new node at a given position.

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,10 +12,10 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-dlistint_t *tmp = *h, *new;
+dlistint_t *tmp = *h, *new_node;
 
 if (idx == 0)
-/*return (add_dnodeint(h, n));*/
+return (add_dnodeint(h, n));
 
 for (; idx != 1; idx--)
 {
@@ -24,18 +24,18 @@ if (tmp == NULL)
 return (NULL);
 }
 
-if (tmp->next == NULL)
-/*return (add_dnodeint_end(h, n));*/
-
-new = malloc(sizeof(dlistint_t));
-if (new == NULL)
+new_node = malloc(sizeof(dlistint_t));
+if (new_node == NULL)
 return (NULL);
 
-new->n = n;
-new->prev = tmp;
-new->next = tmp->next;
-tmp->next->prev = new;
-tmp->next = new;
+new_node->n = n;
+new_node->prev = tmp;
+new_node->next = tmp->next;
 
-return (new);
+if (tmp->next != NULL)
+tmp->next->prev = new_node;
+tmp->next = new_node;
+
+return (new_node);
 }
+
